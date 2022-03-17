6eeeaef2b214adeedd74f3835b4bcd9f2895376e
nabousoum <seynabousoumare98@outlook.fr>
2022-03-17 15:37:36 +0000

    commit du tp1

---
+++ TP1/script.js
@@ -0,0 +1,23 @@
+const right = document.getElementById('right');
+const buttons = document.querySelectorAll('button');
+const container = document.getElementById('container');
+// console.log(buttons);
+
+for(var i=0;i< buttons.length;i++){
+    buttons[i].addEventListener('click',function(){
+        const divNotif = document.createElement('div');
+        divNotif.setAttribute('class','notif');
+        const p = document.createElement('p');
+        p.innerText="Mon projet";
+        divNotif.appendChild(p);
+        right.appendChild(divNotif);
+        var cssObj = window.getComputedStyle(this);
+        //console.log(cssObj.backgroundColor);
+        var color = cssObj.backgroundColor;
+        divNotif.style.backgroundColor = color;
+        setTimeout(() => {
+            divNotif.remove();
+        }, 1000);;
+    })
+}
+
