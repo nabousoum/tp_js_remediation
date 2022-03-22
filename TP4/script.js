0878a108a7f35f730dc9717a144706a8d85032ef
nabousoum <seynabousoumare98@outlook.fr>
2022-03-22 22:03:33 +0000

    commit du tp4 moment

--- TP4/script.js
+++ TP4/script.js
@@ -4,31 +4,25 @@ let hrsBox	= document.querySelector(".hrs");
 let minBox	= document.querySelector(".min");
 let secBox	= document.querySelector(".sec");
 let countDownDate = new Date("Jan 01, 2023 00:01:00").getTime();
+ var interval = 1000;
+ 
+var eventTime= new Date(2023,0,1,00,00,00); 
+var currentTime = new Date(); 
+console.log(currentTime)
+console.log(eventTime)
+//var diffTime = moment("Jan 01, 2023 00:01:00").diff(moment(dateNow),'Days');
+var diffTime = eventTime - currentTime;
+console.log(diffTime);
+var duration = moment.duration(diffTime*1000, 'milliseconds');
 
-let x = setInterval(function() {
-
-	//moment js
-	let now = new Date().getTime();
-	let dist = countDownDate - now;
-	let days 	= Math.floor(dist / (1000 * 60 * 60 * 24));
-	let hours 	= Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	let minutes	= Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
-	let seconds	= Math.floor((dist % (1000 * 60)) / 1000);
-		daysBox.innerHTML = days + "<span>Jours</span>";
-		hrsBox.innerHTML = hours + "<span>Heures</span>";
-		minBox.innerHTML = minutes + "<span>Minutes</span>";
-		secBox.innerHTML = seconds + "<span>Secondes</span>";
-
-}, 1000);
-
-
-// var eventTime= 1366549200; 
-// var currentTime = 1366547400; 
-// var diffTime = eventTime - currentTime;
-// var duration = moment.duration(diffTime*1000, 'milliseconds');
-// var interval = 1000;
-
-// setInterval(function(){
-//   duration = moment.duration(duration - interval, 'milliseconds');
-//     $('.countdown').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
-// }, interval);
+setInterval(function(){
+  duration = moment.duration(duration - interval, 'milliseconds');
+  var d = moment(eventTime).diff(moment(currentTime),'days');
+	var	h = moment.duration(duration).hours();
+	var	m = moment.duration(duration).minutes();
+	var	s = moment.duration(duration).seconds();
+		daysBox.innerHTML = d + "<span>Jours</span>";
+		hrsBox.innerHTML = h + "<span>Heures</span>";
+		minBox.innerHTML = m + "<span>Minutes</span>";
+		secBox.innerHTML = s + "<span>Secondes</span>";
+}, interval);
