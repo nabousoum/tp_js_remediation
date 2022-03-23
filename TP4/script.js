ad64be0f409c3ac5d1668d710360a6f304ab2523
nabousoum <seynabousoumare98@outlook.fr>
2022-03-23 15:01:22 +0000

    styles

--- TP4/script.js
+++ TP4/script.js
@@ -1,28 +1,38 @@
-let countDownBox = document.querySelector(".date");
-let daysBox	= document.querySelector(".days");
-let hrsBox	= document.querySelector(".hrs");
-let minBox	= document.querySelector(".min");
-let secBox	= document.querySelector(".sec");
-let countDownDate = new Date("Jan 01, 2023 00:01:00").getTime();
+const countDownBox = document.querySelector(".date");
+const daysBox	= document.querySelector(".days");
+const hrsBox	= document.querySelector(".hrs");
+const minBox	= document.querySelector(".min");
+const secBox	= document.querySelector(".sec");
+
+// var currentTime = new Date();
+// let eventTime = new Date("Jan 01, 2023 00:01:00");
+// var diffTime = eventTime - currentTime;
+// var duration = moment.duration(diffTime,'milliseconds');
+// var hours = duration._data.hours;
+// var hours = duration._data.hours;
+// var hours = duration._data.hours;
+// console.log(duration);
+
  var interval = 1000;
  
-var eventTime= new Date(2023,0,1,00,00,00); 
+var eventTime= new Date("Jan 01, 2023 00:01:00"); 
 var currentTime = new Date(); 
-console.log(currentTime)
-console.log(eventTime)
-//var diffTime = moment("Jan 01, 2023 00:01:00").diff(moment(dateNow),'Days');
+// console.log(currentTime)
+// console.log(eventTime)
 var diffTime = eventTime - currentTime;
 console.log(diffTime);
-var duration = moment.duration(diffTime*1000, 'milliseconds');
+var duration = moment.duration(diffTime, 'milliseconds');
 
 setInterval(function(){
-  duration = moment.duration(duration - interval, 'milliseconds');
-  var d = moment(eventTime).diff(moment(currentTime),'days');
-	var	h = moment.duration(duration).hours();
-	var	m = moment.duration(duration).minutes();
-	var	s = moment.duration(duration).seconds();
+    duration = moment.duration(duration - interval, 'milliseconds');
+    var d = moment(eventTime).diff(moment(currentTime),'days');
+	var	h = duration._data.hours;
+	var	m = duration._data.minutes;
+	var	s = duration._data.seconds;
 		daysBox.innerHTML = d + "<span>Jours</span>";
 		hrsBox.innerHTML = h + "<span>Heures</span>";
 		minBox.innerHTML = m + "<span>Minutes</span>";
 		secBox.innerHTML = s + "<span>Secondes</span>";
+		//window.location.reload()
 }, interval);
+ 
